246883aa93bc23cb45501b1a9df9208f4f40e263
Proddy <josh@zhbot.org>
2026-07-30 04:20:01 +0100

    feat/Add fur pouch tracking

---
+++ src/main/java/org/zhbot/golem_crafting/FurPouchOverlay.java
@@ -0,0 +1,54 @@
+package org.zhbot.golem_crafting;
+
+import net.runelite.api.Client;
+import net.runelite.api.gameval.ItemID;
+import net.runelite.api.widgets.WidgetItem;
+import net.runelite.client.ui.overlay.*;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+public class FurPouchOverlay extends WidgetItemOverlay {
+    private final Client client;
+    private final GolemCraftingPlugin plugin;
+    //private final GolemCraftingConfig config;
+
+    @Inject
+    private FurPouchOverlay(Client client, GolemCraftingPlugin plugin/*, GolemCraftingConfig config*/)
+    {
+        this.client = client;
+        this.plugin = plugin;
+        //this.config = config;
+
+        showOnInventory();
+        setPriority(2f);
+    }
+
+    @Override
+    public void renderItemOverlay(Graphics2D graphics, int itemId, WidgetItem widgetItem) {
+        if (itemId != ItemID.HG_FURPOUCH_LARGE && itemId != ItemID.HG_FURPOUCH_LARGE_OPEN)
+            return;
+
+        var furPouchCount = plugin.getFurPouchCount();
+        if (furPouchCount > 5)
+            return;
+
+        var bounds = widgetItem.getCanvasBounds();
+        if (bounds == null || bounds.width <= 0 || bounds.height <= 0)
+            return;
+
+        Color color;
+        if (furPouchCount == 0)
+            color = Color.RED;
+        else if (furPouchCount == -1)
+            color = Color.YELLOW;
+        else
+            color = new Color(204, 102, 0);
+
+        graphics.setColor(color);
+        graphics.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
+
+        graphics.setColor(new Color(color.getRed(), color.getGreen(), color.getBlue(), 50));
+        graphics.fillRect(bounds.x, bounds.y, bounds.width, bounds.height);
+    }
+}
--- src/main/java/org/zhbot/golem_crafting/GolemCraftingPlugin.java
+++ src/main/java/org/zhbot/golem_crafting/GolemCraftingPlugin.java
@@ -7,14 +7,19 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.events.*;
+import net.runelite.api.gameval.InterfaceID;
 import net.runelite.api.gameval.InventoryID;
 import net.runelite.api.gameval.ItemID;
+import net.runelite.api.widgets.Widget;
 import net.runelite.client.Notifier;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
+
+import java.util.Set;
+import java.util.regex.Pattern;
 
 @Slf4j
 @PluginDescriptor(
@@ -29,11 +34,31 @@ public class GolemCraftingPlugin extends Plugin
 	private static final int SOUTH_GOLEM_WEST_STATE_ID = 15737;
 	//private static final int GOLEMS_CRAFTED_TOTAL_ID = 15738;
 
+	private static final Pattern FUR_POUCH_PATTERN = Pattern.compile("Your fur pouch is currently holding (\\d+) fur\\.");
+	private static final Set<Integer> FUR_ITEM_IDS = Set.of(
+			ItemID.HUNTINGBEAST_POLAR_FUR,
+			ItemID.HUNTINGBEAST_WOODLAND_FUR,
+			ItemID.HUNTINGBEAST_JUNGLE_FUR,
+			ItemID.HUNTINGBEAST_DESERT_FUR,
+			ItemID.HUNTING_FENNECFOX_FUR,
+			ItemID.HUNTING_FUR_JAGUAR_PERFECT,
+			ItemID.HUNTING_FUR_LEOPARD_PERFECT,
+			ItemID.HUNTING_FUR_TIGER_PERFECT,
+			ItemID.HUNTING_ANTELOPESUN_FUR,
+			ItemID.HUNTING_ANTELOPEMOON_FUR,
+			ItemID.HUNTINGBEAST_SPEEDY_FUR,
+			ItemID.HUNTINGBEAST_SILENT_FUR,
+			ItemID.HUNTINGBEAST_SPEEDY2_FUR,
+			/*ItemID.GOAT_PIT_FUR*/34017
+	);
+
 	@Inject
 	private Client client;
 
 	@Inject
 	private GolemCraftingConfig config;
+
+	@Inject ConfigManager configManager;
 
 	@Inject
 	private Notifier notifier;
@@ -43,6 +68,9 @@ public class GolemCraftingPlugin extends Plugin
 
 	@Inject
 	private SouthGolemOverlay southGolemOverlay;
+
+	@Inject
+	private FurPouchOverlay furPouchOverlay;
 
 	@Getter
 	private int southGolemProgress;
@@ -62,16 +90,37 @@ public class GolemCraftingPlugin extends Plugin
 	@Getter
 	private boolean southGolemWestDone;
 
+	private static final String FUR_POUCH_KEY = "furPouchCount";
+	public int getFurPouchCount()
+	{
+		var accountHash = client.getAccountHash();
+		if (accountHash == -1)
+			return -1;
+
+		Integer value = configManager.getConfiguration(GolemCraftingConfig.group, FUR_POUCH_KEY + "_" + accountHash, Integer.class);
+		return value != null ? value : -1;
+	}
+	private void setFurPouchCount(int value)
+	{
+		var accountHash = client.getAccountHash();
+		if (accountHash == -1)
+			return;
+
+		configManager.setConfiguration(GolemCraftingConfig.group, FUR_POUCH_KEY + "_" + accountHash, value);
+	}
+
 	@Override
 	protected void startUp() throws Exception
 	{
 		overlayManager.add(southGolemOverlay);
+		overlayManager.add(furPouchOverlay);
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
 		overlayManager.remove(southGolemOverlay);
+		overlayManager.remove(furPouchOverlay);
 	}
 
 	@Subscribe
@@ -84,6 +133,8 @@ public class GolemCraftingPlugin extends Plugin
 				southGolemProgressTick = client.getTickCount();
 				if (event.getValue() > 1)
 					notifier.notify(config.notification(), "Golem stage complete");
+				else if (event.getValue() == 0 && getFurPouchCount() > 0 && hasLargeFurPouch())
+					setFurPouchCount(getFurPouchCount() - 1);
 				break;
 			case SOUTH_GOLEM_NORTH_STATE_ID:
 				southGolemNorthDone = event.getValue() != 0;
@@ -97,6 +148,69 @@ public class GolemCraftingPlugin extends Plugin
 			case SOUTH_GOLEM_WEST_STATE_ID:
 				southGolemWestDone = event.getValue() != 0;
 				break;
+		}
+	}
+
+	@Subscribe
+	public void onChatMessage(ChatMessage event)
+	{
+		if (event.getType() != ChatMessageType.GAMEMESSAGE)
+			return;
+		if (!hasLargeFurPouch())
+			return;
+
+		var message = event.getMessage();
+
+		if (message.contains("You need to dress the golem in furs from hunted creatures.") ||
+			message.contains("Your fur pouch is empty."))
+		{
+			setFurPouchCount(0);
+			return;
+		}
+
+		var matcher = FUR_POUCH_PATTERN.matcher(message);
+		if (!matcher.find())
+			return;
+
+		setFurPouchCount(Integer.parseInt(matcher.group(1)));
+	}
+
+	@Subscribe
+	public void onMenuOptionClicked(MenuOptionClicked event)
+	{
+		var itemId = event.getItemId();
+		if (itemId != ItemID.HG_FURPOUCH_LARGE && itemId != ItemID.HG_FURPOUCH_LARGE_OPEN)
+			return;
+
+		ItemContainer inventory = client.getItemContainer(InventoryID.INV);
+		if (inventory == null)
+			return;
+
+		switch (event.getMenuOption())
+		{
+			case "Fill":
+				var furCount = 0;
+				for (var item : inventory.getItems())
+					if (FUR_ITEM_IDS.contains(item.getId()))
+						furCount++;
+
+				setFurPouchCount(Math.min(28, getFurPouchCount() + furCount));
+				break;
+			case "Empty":
+				if (isBankOpen())
+				{
+					setFurPouchCount(0);
+					break;
+				}
+
+				var freeSpots = 0;
+				for (var item : inventory.getItems())
+					if (item.getId() == -1)
+						freeSpots++;
+
+				setFurPouchCount(Math.max(0, getFurPouchCount() - freeSpots));
+			case "Empty-to-bank": // Item Charges Improved
+				setFurPouchCount(0);
 		}
 	}
 
@@ -131,8 +245,25 @@ public class GolemCraftingPlugin extends Plugin
 		if (inventory.count(34020/*Sunstone*/) < 4)
 			return false;
 
-		// TODO: Check fur pouch
+		if (hasLargeFurPouch() && getFurPouchCount() == 0)
+			return false;
 
 		return true;
+	}
+
+	public boolean hasLargeFurPouch()
+	{
+		ItemContainer inventory = client.getItemContainer(InventoryID.INV);
+		if (inventory == null)
+			return false;
+
+		return inventory.contains(ItemID.HG_FURPOUCH_LARGE) || inventory.contains(ItemID.HG_FURPOUCH_LARGE_OPEN);
+	}
+
+	public boolean isBankOpen()
+	{
+		Widget bankWidget = client.getWidget(InterfaceID.BANKMAIN);
+
+		return bankWidget != null && !bankWidget.isHidden();
 	}
 }
